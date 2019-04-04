f0cd6972b97dcb4c47d4bfc691be837f70570f75
cbleser <cr@tagion.com>
2019-04-04 19:17:07 +0300

    Docker /dev/tty problem fixed

--- tagion/Options.d
+++ tagion/Options.d
@@ -111,6 +111,8 @@ struct Options {
     bool trace_gossip;
     // Directory for the trace files
     string tmp;
+    // Print output
+    string stdout;
     //Port for network socket
     ushort network_socket_port;
     // Sequential test mode
@@ -190,3 +192,42 @@ struct Options {
 }
 
 __gshared static Options options;
+
+struct TransactionMiddlewareOptions {
+    // port for the socket
+    ushort port;
+    // address for the socket
+    string address;
+    //  port for the socket to the tagion network
+    ushort network_port;
+    //  address for the socket to the tagion network
+    string network_address;
+
+    string logext;
+    string logname;
+    string logpath;
+
+    mixin JSONCommon;
+
+    void parseJSON(string json_text) {
+        auto json=JSON.parseJSON(json_text);
+        parse(json);
+    }
+
+    void load(string config_file) {
+        if (config_file.exists) {
+            auto json_text=readText(config_file);
+            parseJSON(json_text);
+        }
+        else {
+            save(config_file);
+        }
+    }
+
+    void save(string config_file) {
+        config_file.write(stringify);
+    }
+
+}
+
+__gshared static TransactionMiddlewareOptions transaction_middleware_options;
