274d0fbc19596db2ab2f0e1f05fe7eecd4a66dc4
Vlad Pazych <vladpazych@gmail.com>
2021-11-17 13:54:56 +0200

    wip: Simplify context structure

--- context.mk
+++ context.mk
@@ -1,6 +1,3 @@
 DEPS += lib-logger
 
-configure-libbasic.a: SOURCE := tagion/**/*.d tagion/*.d
-configure-libbasictest: SOURCE := tagion/**/*.d tagion/*.d
-
-# $(DBIN)/libbasic.a: INFLILES += $(DTMP)/libsecp256k1.a
+$(DBIN)/libbasic.a: SOURCE := tagion/**/*.d
